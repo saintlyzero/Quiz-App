ec68dbf0949ffa67b3012e5871cbb7f67c818c1a
Shubham Dalvi <shubham.dalvi@quantiphi.com>
2019-06-15 01:37:48 +0530

    added fetch questions API

---
+++ getQuestions.php
@@ -0,0 +1,39 @@
+<?php
+
+include('connect.php');
+
+    $username=$_POST['username'];
+    $category=$_POST['topic'];
+    $subset  = 5;
+
+    $sql = "SELECT * from ".$category." where qid > 
+    (SELECT qno from up where user_name = '$username' and category = '$category') 
+    limit ".$subset;
+
+    $result = mysqli_query($conn,$sql);
+    
+    $response = array();
+
+    if(mysqli_num_rows($result)!=0)
+    {
+
+        while($row = mysqli_fetch_array($result))
+            {
+                $temp = array(); 
+                $temp['qid']=$row['qid'];
+                $temp['question']=$row['question'];
+                $temp['opt1']=$row['opt1'];
+                $temp['opt2']=$row['opt2'];
+                $temp['opt3']=$row['opt3'];
+                $temp['opt4']=$row['opt4'];
+                $temp['answer']=$row['answer'];
+                
+                array_push($response,$temp);
+            }
+            echo json_encode($response);
+    }
+    else
+    {
+        echo "No data";
+    }
+?>
