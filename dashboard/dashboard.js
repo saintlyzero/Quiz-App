f6a53e277cad45373bc066c3c48194707877aaf6
Shubham Dalvi <shubham.dalvi@quantiphi.com>
2019-06-15 19:54:48 +0530

    Done UI fixed and Ajax calls

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -22,14 +22,58 @@ function nav_close() {
     overlayBg.style.display = "none";
 }
 
-function leaderboard()
-{
-    document.getElementById("main-block").style.display = 'none';
-    document.getElementById("leaderboards-block").style.display = 'block';
-    
-}
+// function leaderboard()
+// {
+//     document.getElementById("main-block").style.display = 'none';
+//     document.getElementById("leaderboards-block").style.display = 'block';
+//     document.getElementById("quiz-block").style.display = 'none';
+
+
+//     $.ajax({
+//         url: 'http://localhost/quiz-app/leaderBoards.php',
+//         type: 'GET',
+//         dataType:'json',
+//         // data: { "username": readCookie('username') },
+//         success: function(response) { 
+//         console.log(response);
+//         res_json = response;//JSON.parse(JSON.stringify(response));
+//         if (res_json["status"] == 1)
+//         {
+//         console.log(res_json);
+//         }
+//         else
+//         {
+//         console.log(res_json['message']);
+//         }
+//         },
+//         error: function(data){
+//         console.log(data);
+//         console.log("Error in api call of get score for user.");
+//         }
+//         });
+
+// }
 function dashboard()
 {
     document.getElementById("main-block").style.display = 'block';
     document.getElementById("leaderboards-block").style.display = 'none';
+    document.getElementById("quiz-block").style.display = 'none';
+}
+function loadQuiz(category)
+{
+    document.getElementById("main-block").style.display = 'none';
+    document.getElementById("leaderboards-block").style.display = 'none';
+    document.getElementById("quiz-block").style.display = 'block';
+    
+        // Get value from input element on the page
+        var numValue = $("#num").val();
+       
+        // Send the input data to the server using get
+        $.post("http://localhost/quiz-app/getQuestions.php", {username: 'abc',topic:'aptitude'} , function(data){
+            // Display the returned data in browser
+            console.log(data);
+            // $("#result").html(data);
+        });
+   
+    
 }
